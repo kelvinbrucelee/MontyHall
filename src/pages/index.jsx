6dc6e7f662a3f71d78c8fc8c559b80c4ea27f565
kelvinbrucelee <kelvinbrucelee@hotmail.com>
2023-04-27 00:59:01 -0300

    Iniciando Projeto

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import PortaModel from "../model/porta";
+import Porta from "../components/Porta";
+import { useState } from "react";
+
+export default function Home() {
+  const  [p1, setP1] = useState(new PortaModel(1))
+
+  return (
+    <div style={{display: "flex", flexDirection: "column"}}>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
+    </div>
+  )
+}
